7141f53a13ae76b4ab5e71dcd77634d10e7033f2
Ilia Kohanovski <adsurbum@gmail.com>
2023-12-20 19:02:47 +0200

    fixed addTwo

--- AddTwo.java
+++ AddTwo.java
@@ -3,7 +3,8 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		System.out.println("hello2");
-		dsfdsfds	
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+		System.out.println(num1+num2);
 	}
 }
